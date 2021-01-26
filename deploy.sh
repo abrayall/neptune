5a121de5364d1136e9dc90198fd860b56eceef1d
Aaron Brayall <abrayall@yahoo.com>
2021-01-26 17:58:20 -0500

    refactoring so each service is in its own directory2

--- deploy.sh
+++ deploy.sh
@@ -8,14 +8,14 @@ version=$version-$(date '+%Y%m%d%H%M')
 echo Deploying neptune...
 echo Deploying neptune-agent into minikube...
 minikube cache add neptune-agent:$version
-kubectl apply -f build/work/resources/kubernetes/agent.yaml
+kubectl apply -f build/work/agent/resources/kubernetes/agent.yaml
 
 echo Deploying neptune-timeseries into minikube...
 minikube cache add neptune-timeseries:$version
-kubectl apply -f build/work/resources/kubernetes/timeseries.yaml
+kubectl apply -f build/work/timeseries/resources/kubernetes/timeseries.yaml
 
 echo Deploying neptune-inventory into minikube...
-kubectl apply -f build/work/resources/kubernetes/inventory.yaml
+kubectl apply -f build/work/inventory/resources/kubernetes/inventory.yaml
 
 echo Waiting for services to start...
 while [ `kubectl get pods | grep 1/1 | wc -l` -ne 2 ]; do
